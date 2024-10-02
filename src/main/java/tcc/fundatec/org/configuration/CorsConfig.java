9e9e9a00b1c1d4dfd36f1f74936d8dcc3721bef8
Bassetti97 <gabybasetti3@gmail.com>
2024-10-02 15:35:15 -0300

    atualização de configuração

--- src/main/java/tcc/fundatec/org/configuration/CorsConfig.java
+++ src/main/java/tcc/fundatec/org/configuration/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registro){
         registro.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE");
     }
 }
